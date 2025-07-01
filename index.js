14dabf2c76e9963b8c6af052be66218a846caaf4
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2025-07-01 08:01:37 +0100

    Added background images to the drums

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-alert("hello world"); 
+// JAVASCRIPT CODE 
+
+// Selec all the drums, and loop through, & addevent listener
+const drums = document.querySelectorAll(".drum"); 
+const numberOfDrums = drums.length; 
+
+for (let i = 0; i < numberOfDrums; i++) {
+    drums[i].addEventListener("click", function() {
+        const audio = new Audio("../assets/sound_samples/tom-1.mp3")
+        audio.play();
+    })
+}
