7f392937b7f290490c5d23a1129bbbef8119dc4d
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2025-07-01 23:09:26 +0100

    Make the sounds work

--- index.js
+++ index.js
@@ -4,9 +4,55 @@
 const drums = document.querySelectorAll(".drum"); 
 const numberOfDrums = drums.length; 
 
+
+// To detect drum element clicked
 for (let i = 0; i < numberOfDrums; i++) {
     drums[i].addEventListener("click", function() {
-        const audio = new Audio("../assets/sound_samples/tom-1.mp3")
-        audio.play();
+
+        const buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML); 
     })
 }
+
+
+// To play the sound tapped
+function makeSound(key) {
+    switch (key) {
+        case "w":
+        const tom1Audio = new Audio("../assets/sound_samples/tom-1.mp3")
+        tom1Audio.play(); 
+        break;
+        case "a":
+        const tom2Audio = new Audio("../assets/sound_samples/tom-2.mp3")
+        tom2Audio.play(); 
+        break;
+        case "s":
+        const tom3Audio = new Audio("../assets/sound_samples/tom-3.mp3")
+        tom3Audio.play(); 
+        break;
+        case "d":
+        const tom4Audio = new Audio("../assets/sound_samples/tom-4.mp3")
+        tom4Audio.play(); 
+        break;
+        case "j":
+        const snareAudio = new Audio("../assets/sound_samples/snare.mp3")
+        snareAudio.play(); 
+        break;
+        case "k":
+        const crashAudio = new Audio("../assets/sound_samples/crash.mp3")
+        crashAudio.play(); 
+        break;
+        case "l":
+        const kickAudio = new Audio("../assets/sound_samples/kick-bass.mp3")
+        kickAudio.play(); 
+        break;  
+    }     
+}
+
+
+// To detect key being pressed.
+document.addEventListener("keydown", function(event) {
+        
+    makeSound(event.key); 
+
+})
